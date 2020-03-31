30dbdd7d8caa4a7d8a2043020c0397680bf485c1
Hasan Demirtaş <utsukushihito@outlook.com>
2020-04-01 00:32:11 +0300

    Update MapItemBuilder.java

--- src/main/java/io/github/portlek/bukkititembuilder/MapItemBuilder.java
+++ src/main/java/io/github/portlek/bukkititembuilder/MapItemBuilder.java
@@ -1,7 +1,10 @@
 package io.github.portlek.bukkititembuilder;
 
+import org.bukkit.Color;
 import org.bukkit.inventory.meta.MapMeta;
+import org.bukkit.map.MapView;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public final class MapItemBuilder extends Builder<MapItemBuilder, MapMeta> {
 
@@ -14,6 +17,30 @@ public final class MapItemBuilder extends Builder<MapItemBuilder, MapMeta> {
     public MapItemBuilder mapId(final int id) {
         return this.change(() ->
             this.meta.setMapId(id));
+    }
+
+    @NotNull
+    public MapItemBuilder mapView(@NotNull final MapView mapView) {
+        return this.change(() ->
+            this.meta.setMapView(mapView));
+    }
+
+    @NotNull
+    public MapItemBuilder scaling(final boolean scaling) {
+        return this.change(() ->
+            this.meta.setScaling(scaling));
+    }
+
+    @NotNull
+    public MapItemBuilder locationName(@Nullable final String name) {
+        return this.change(() ->
+            this.meta.setLocationName(name));
+    }
+
+    @NotNull
+    public MapItemBuilder color(@Nullable final Color color) {
+        return this.change(() ->
+            this.meta.setColor(color));
     }
 
     @Override
