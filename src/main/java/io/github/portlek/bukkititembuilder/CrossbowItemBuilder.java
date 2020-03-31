a4da03ea16bdeff2bd2e644595a529703c68452d
Hasan Demirtaş <utsukushihito@outlook.com>
2020-03-31 23:44:44 +0300

    Create CrossbowItemBuilder.java

---
+++ src/main/java/io/github/portlek/bukkititembuilder/CrossbowItemBuilder.java
@@ -0,0 +1,51 @@
+package io.github.portlek.bukkititembuilder;
+
+import java.util.Arrays;
+import java.util.List;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.CrossbowMeta;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public final class CrossbowItemBuilder {
+
+    @NotNull
+    private final ItemStackBuilder builder;
+
+    @NotNull
+    private final CrossbowMeta crossbowMeta;
+
+    public CrossbowItemBuilder(@NotNull final ItemStackBuilder builder, @NotNull final CrossbowMeta crossbowMeta) {
+        this.builder = builder;
+        this.crossbowMeta = crossbowMeta;
+    }
+
+    @NotNull
+    public CrossbowItemBuilder chargedProjectiles(@Nullable final ItemStack... projectiles) {
+        return this.chargedProjectiles(Arrays.asList(projectiles));
+    }
+
+    @NotNull
+    public CrossbowItemBuilder chargedProjectiles(@Nullable final List<ItemStack> projectiles) {
+        this.crossbowMeta.setChargedProjectiles(projectiles);
+        this.change();
+        return this;
+    }
+
+    private void change() {
+        this.builder.setItemMeta(this.crossbowMeta);
+    }
+
+    @NotNull
+    public CrossbowItemBuilder chargedProjectile(@Nullable final ItemStack projectile) {
+        this.crossbowMeta.addChargedProjectile(projectile);
+        this.change();
+        return this;
+    }
+
+    @NotNull
+    public ItemStack build() {
+        return this.builder.build();
+    }
+
+}
